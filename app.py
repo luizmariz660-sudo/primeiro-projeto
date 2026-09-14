ae95ba02e11be2c67318646e30fba522928946c5
luizmariz <luizmariz660@gmail.com>
2026-09-14 10:31:44 -0300

    versão inicial (app.py e README).

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-nome = input("Digite o nome do aparelho: ")
-potencia = float(input("Digite a potência do aparelho em watts (W): "))
-horasDia = float(input("Digite o tempo médio de uso diário em horas: "))
+valor_compra = float(input("Digite o valor total da compra: R$ "))
 
-consumoMensal = (potencia * horasDia * 30) / 1000
+if valor_compra < 200:
+    percentual_desconto = 0.05
+elif valor_compra < 300:
+    percentual_desconto = 0.10
+else:
+    percentual_desconto = 0.15
 
-print(f"\nAparelho: {nome}")
-print(f"Consumo mensal: {consumoMensal:.2f} kWh")
+valor_desconto = valor_compra * percentual_desconto
+valor_final = valor_compra - valor_desconto
+
+print(f"\nValor da compra: R$ {valor_compra:.2f}")
+print(f"Desconto aplicado: R$ {valor_desconto:.2f}")
+print(f"Total a pagar: R$ {valor_final:.2f}")
+
